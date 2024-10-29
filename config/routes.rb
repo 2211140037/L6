4b0e89f4c441d05eec7f58586b2ad1e7ed77dda2
Tokiya Fukuda <2211140037x@ed.fuk.kindai.ac.jp>
2024-10-29 07:12:36 +0000

    L6

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'carts/show'
+  resources:products
+  get"top/main"
+  resources:cartitems,only:[:new,:create,:destroy]
+  resources:carts,only:[:show]
+  root "top#main"
+end
